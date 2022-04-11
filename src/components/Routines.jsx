8591a7c54c86df9c0430a311af4b618646ae54f8
JeongwookNa <najung936@hotmail.com>
2022-04-11 13:09:13 -0500

    fix

--- src/components/Routines.jsx
+++ src/components/Routines.jsx
@@ -1,32 +1,54 @@
 import { useState, useEffect } from "react";
-import {URL} from './api/index'
+
 
 const Routine = () => {
-    const [fetchroutine, setFetchRoutine] = useState([]);
+    const [routines, setRoutines] = useState([]);
 
 
 const getRoutines = async () => {
-    const fetchRoutines = await fetch(
-      `${URL}routines`,
+    const fetchRoutines = await fetch('http://fitnesstrac-kr.herokuapp.com/api/routines',
       {
         headers: {
           "Content-Type": "application/json",
         },
       }
     );
-    const response = await fetchroutine.json();
+    const response = await fetchRoutines.json();
     console.log(response);
-    setFetchRoutine(response);
+    setRoutines(response);
   };
   useEffect(() => {
+    
     getRoutines();
   }, []);
 
-}
+  
+  return (
+    <div>
+    <>
+      <h1>Routines</h1>
+      {routines ? routines.map((routine, i) => {
+          console.log("routines: ", routines);
+        return (
+          <div key={i}>
+            <h3>{routine.name}</h3>
+            <div>{routine.description}</div>
+            <div>{routine.goal}</div>
+            <div>{routine.creatorId}</div>
+          </div>
+        );
+      }): null} 
+    </>
+  </div>
+);
+};
 
 
-}).then(response => response.json())
-.then(result => {
-  console.log(result);
-})
-.catch(console.error);
+
+// }).then(response => response.json())
+// .then(result => {
+  //   console.log(result);
+  // })
+  // .catch(console.error);
+
+  export default Routine;
