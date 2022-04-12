46e90d68327b7d3765446ef1557bf3a6ee3e6c06
Aheiy1 <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>
2022-04-12 13:22:50 -0500

    my routines

--- src/components/MyRoutines.jsx
+++ src/components/MyRoutines.jsx
@@ -1,11 +1,61 @@
-import React from 'react';
 
-const MyRoutines = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
 
-export default MyRoutines;
+import React, { useEffect, useState } from "react";
+// import { DeletePost } from "./index.js";
+
+const MyRoutines = ({ routines }) => {
+  const [myRoutines, setMyRoutines] = useState([]);
+  const storedName = localStorage.getItem("username");
+
+  const makeMyRoutines = async () => {
+    try {
+      const filteredResult = routines.filter(
+        (routine) => routine.creatorName === `${storedName}`
+      );
+      setMyRoutines(filteredResult);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    makeMyRoutines();
+  }, []);
+
+  function userTernary() {
+    if (storedName) {
+      return (
+        <>
+          <h1 className="welcomeText">Welcome {storedName}</h1>
+          <h2>{"My Routines:"}</h2>
+          <div className="cardField">
+            {myRoutines.map((myRoutine) => {
+              return (
+                <>
+                  <div className="postCard" key={`${myRoutine._id} Profile`}>
+                    <div className="title">{myRoutine.name}</div>
+                    <div className="author">
+                      Owner:
+                      <div id="author">{myRoutine.creatorName}</div>
+                    </div>
+                    <div className="description">
+                      Description:
+                      <div id="description">{myRoutine.goal}</div>
+                    </div>
+                   
+                  </div>
+                </>
+              );
+            })}
+          </div>
+        </>
+      );
+    } else {
+      return <h1>Please Login</h1>;
+    }
+  }
+
+  return userTernary();
+
+}
+export default MyRoutines
