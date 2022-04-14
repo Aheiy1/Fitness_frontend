d7e18e87f366cbc764c22bd5ff1dcef7ae7c5416
Aheiy1 <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>
2022-04-14 18:24:19 -0500

    update routine is working

--- src/components/UpdateRoutine.jsx
+++ src/components/UpdateRoutine.jsx
@@ -1,16 +1,21 @@
+import React, { useState } from "react";
 
-import React, {useState} from 'react';
-
-const UpdateRoutine = ({routines, setRoutines, routineId, setRoutineId, myRoutines, setMyRoutines}) => {
-    
-    const [name, setName] = useState([]);
-  const [description, setDescription] = useState([]);
+const UpdateRoutine = ({
+  routines,
+  setRoutines,
+  routineId,
+  setRoutineId,
+  myRoutines,
+  setMyRoutines,
+}) => {
+  const [name, setName] = useState([]);
+  const [goal, setGoal] = useState([]);
   const token = localStorage.getItem("token");
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     console.log("name", name);
-    console.log("description", description);
+    console.log("description", goal);
     const response = await fetch(
       `http://fitnesstrac-kr.herokuapp.com/api/routines/${routineId}`,
       {
@@ -20,57 +25,54 @@ const UpdateRoutine = ({routines, setRoutines, routineId, setRoutineId, myRoutin
           Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({
-          name : name,
-          description: description
+          name: name,
+          goal: goal,
         }),
       }
     );
 
     const data = await response.json();
-  
+
     if (data && data.name) {
-        console.log(myRoutines)
+      console.log(myRoutines);
       const newRoutines = myRoutines.map((routine) => {
         if (routine.id === routineId) {
-            console.log(data)
+          console.log(data);
           return data;
         } else {
           return routine;
         }
       });
-      console.log(newRoutines)
-      setMyRoutines([...myRoutines, newRoutines])
-      console.log(myRoutines)
-      setName("");
-      setDescription("");
+      console.log(newRoutines);
+      setMyRoutines(newRoutines);
+      console.log(myRoutines);
+      setGoal("");
       setRoutineId(null);
     }
   };
-    
-    
-    
-    return (
-        <>
-          <h3>Update my Routine</h3>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              placeholder="Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            ></input>
-            <input
-              type="text"
-              placeholder="Description"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            ></input>
-            <button type="submit" className="btn">
-              Submit
-            </button>
-          </form>
-        </>
-      );
+
+  return (
+    <>
+      <h3>Update my Routine</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+        <input
+          type="text"
+          placeholder="Description"
+          value={goal}
+          onChange={(e) => setGoal(e.target.value)}
+        ></input>
+        <button type="submit" className="btn">
+          Submit
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default UpdateRoutine;
